36aa5d10d66b2f675b4703829481daa8cab97290
PaanJoe <farhan.gunz@gmail.com>
2022-10-23 01:37:29 +0800

    remove random text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,6 @@ import Parallax from '@/components/home/parallax';
 export default function HomePage() {
   return (
     <Layout>
-      test
       <Seo templateTitle='Home' />
       <main>
         <section>
